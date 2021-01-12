cc623485e8fd63572d17919a3c8359903d6d8fbe
saulo775 <59702227+saulo775@users.noreply.github.com>
2021-01-11 22:24:31 -0300

    estilo global

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div``;
+
+export const Background = styled.div``;
