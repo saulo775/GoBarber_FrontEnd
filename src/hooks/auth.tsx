8da11e7fff144951a2ffda74d9cfabd46197f445
saulo775 <59702227+saulo775@users.noreply.github.com>
2021-03-02 11:39:03 -0300

    TDD - Finalizando testes no hook de autenticação

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -87,9 +87,7 @@ const AuthProvider: React.FC = ({ children }) => {
 
 function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuth must be used whitin an AuthProvider');
-  }
+
   return context;
 }
 
